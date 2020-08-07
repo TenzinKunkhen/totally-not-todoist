89516eb4ab024c21060bfbaf371db3166c6bb6dc
TenzinKunkhen <kunkhen.tenzin@gmail.com>
2020-08-07 17:14:01 -0400

    can get data properly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Header } from './components/layout/Header';
-import { Sidebar } from './components/layout/Sidebar';
+import { Content } from './components/layout/Content';
 
 export const App = () => (
   <div className="App">
     <header className="App-header">
-      <p> hello </p>
+      <p> hell </p>
       <Header />
-      <Sidebar />
+      <Content />
     </header>
   </div>
 );
