55e36e76f86a15c14384279d0214abbc24f69517
TenzinKunkhen <kunkhen.tenzin@gmail.com>
2020-08-23 14:26:39 -0400

    Updates

---
+++ precache-manifest.f950d275cf1a443d4ae2d86155e36960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a2a5f9c7694edb48076b9ccd323a42",
+    "url": "/totally-not-todoist/index.html"
+  },
+  {
+    "revision": "d9b6cd98427b728e5d07",
+    "url": "/totally-not-todoist/static/css/main.a8b7cbcd.chunk.css"
+  },
+  {
+    "revision": "30e173de775b5a0da2b0",
+    "url": "/totally-not-todoist/static/js/2.bd3b23e0.chunk.js"
+  },
+  {
+    "revision": "052157e0581e32a2b8fa8f7f1414d257",
+    "url": "/totally-not-todoist/static/js/2.bd3b23e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b6cd98427b728e5d07",
+    "url": "/totally-not-todoist/static/js/main.c2773fdb.chunk.js"
+  },
+  {
+    "revision": "fceefa8509dcfbe58c75",
+    "url": "/totally-not-todoist/static/js/runtime-main.9024e078.js"
+  }
+]);
